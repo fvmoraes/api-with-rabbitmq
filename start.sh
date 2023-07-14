fceecf92da1960ae2f77714d610136b995375333
Franklin Moraes <eng.fvmoraes@gmail.com>
2023-07-14 20:34:58 -0300

    fix: readonly to erlang.cookie

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@
 # docker stop $(docker ps -qa) && docker system prune -af --volumes
 ##Adjust permission to posgres-data (host volume) and run compose
 sudo chmod -R 777 ./build/volumes/*
+sudo chmod 400 ./build/volumes/rabbitmq_data/.erlang.cookie
 docker compose up
