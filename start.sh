f375eeffac87b3a79f244d3e042993fb79df3f0b
Franklin Moraes <eng.fvmoraes@gmail.com>
2023-07-16 21:37:51 -0300

    fix: adjust comments

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 ##Docker objects cleaner
 # docker stop $(docker ps -qa) && docker system prune -af --volumes
-##Adjust permission to posgres-data (host volume) and run compose
+##Adjust permission to posgres_data and rabbitmq_data (host volume) and run compose
 sudo chmod -R 777 ./build/volumes/*
 sudo chmod 400 ./build/volumes/rabbitmq_data/.erlang.cookie
 docker compose up
